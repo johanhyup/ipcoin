a2c36fb252fac874bce1f3e87cce5303ec2400da
johanhyup <choup5525@gmail.com>
2025-03-03 18:14:37 +0900

    footer.php 업데이트

--- ipcoin/master/frames/footer.php
+++ ipcoin/master/frames/footer.php
@@ -1,17 +1,16 @@
-    </section> <!-- /.content -->
-  </div> <!-- /.content-wrapper -->
+    </div> <!-- /.content-wrapper -->
 
-  <!-- 하단 Footer -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-inline">
-      <b>Version</b> 1.0.0
-    </div>
-    <strong>
-      Copyright &copy;
-      <?php echo date('Y'); ?>
-      <a href="#" style="color: #4CAF50; text-decoration: none;"> IPcoin</a>.
-    </strong> All rights reserved.
-  </footer>
+    <!-- 하단 Footer -->
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-inline">
+            <b>Version</b> 1.0.0
+        </div>
+        <strong>
+            Copyright &copy;
+            <?php echo date('Y'); ?>
+            <a href="#" style="color: #4CAF50; text-decoration: none;"> IPcoin</a>.
+        </strong> All rights reserved.
+    </footer>
 </div> <!-- ./wrapper -->
 
 <!-- AdminLTE 스크립트 -->
